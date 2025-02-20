8b51c0f067e3497fb8f95bf8078b1f2d6af6360b
Matheus Maximiliano <maximiliano.mprado@gmail.com>
2025-02-20 19:22:22 -0300

    add CategorizeImageFunction with Rekognition integration and requirements

---
+++ categorize/app.py
@@ -0,0 +1,31 @@
+import json
+import boto3 # Importando a biblioteca boto3 para trabalhar com os serviços da AWS
+import os # Para recuperar variáveis de ambiente
+
+rekognition_client = boto3.client("rekognition") # Instanciando o cliente do Rekognition
+
+def lambda_handler(event, context):
+    
+    # Captura do Evento de Put do Amazon S3
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    file_name = event['Records'][0]['s3']['object']['key']
+    
+
+    # Chama do evento de detecção de labels do rekognition
+    response = rekognition_client.detect_labels(
+        Image={'S3Object': {'Bucket':bucket_name, 'Name': file_name }},
+        MaxLabels=10,
+        MinConfidence=80
+    )
+    
+    # Lista de labels detectadas
+    labels = [label['Name'] for label in response['Labels']]
+    
+    print(labels)
+    
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "Processamento de Labels realizado com sucesso",
+        }),
+    }
