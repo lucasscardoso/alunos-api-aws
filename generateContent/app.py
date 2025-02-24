ad728f6e19cd2237718dfbdb08a205ce60d43d41
Matheus Maximiliano <maximiliano.mprado@gmail.com>
2025-02-24 19:42:38 -0300

    add invoke_bedrock function to integrate Bedrock ML model invocation

--- generateContent/app.py
+++ generateContent/app.py
@@ -9,5 +9,35 @@ prompt_title = os.environ['PROMPT_TITLE'] # Título do prompt criado no Bedrock
 prompt_description = os.environ['PROMPT_DESCRIPTION'] # Descrição do prompt criado no Bedrock
 
 
+def invoke_bedrock(prompt):
+    # Função para invocar o modelo de Machine Learning criado no Bedrock
+    request = {
+        "anthropic_version": "bedrock-2023-05-31",
+        "max_tokens": 500,
+        "temperature": 0.5,
+        "messages":[
+            {
+                "role":"user",
+                "content": [{"type":"text", "text": prompt}]
+            }
+        ]
+    }
+    
+    # Carrega o request em formato JSON
+    json_request = json.dumps(request)
+    
+    # Invoca o modelo de Machine Learning
+    response = bedrock_client.invoke_model(modelId=model_id, body=json_request)
+    
+    # Carrega a resposta em formato JSON
+    model_response = json.loads(response['body'].read())
+    
+    # Recupera o texto da resposta
+    text_response = model_response['content']['0']['text']
+    
+    print (text_response)
+    
+    return text_response
+
 def lambda_handler(event, context):
     
